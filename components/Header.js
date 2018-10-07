08c4628904dbca65ccd7c117b0ab9472b2b292ff
Michael Lynn <merlynn@gmail.com>
2018-10-07 09:34:20 -0400

    Change Header, add show 001

--- components/Header.js
+++ components/Header.js
@@ -2,21 +2,24 @@ import Subscribe from './Subscribe';
 
 const Header = () => (
   <header className="header">
-    <div className="header__left">
+    <div className="header__center">
       <h1 aria-label="journalpodcast.com">
         <img className="header__logo" src="/static/logo.png" alt="Journal" />
       </h1>
-    </div>
-    <div className="header__right">
-      <div className="title">
-        <h2 className="tagline">The MongoDB Podcast</h2>
-        <a
+      <span className="tagLine">
+      <a
           target="_blank"
           href="https://goo.gl/forms/mgBf4ngRJmSx7Gcz2"
           className="title__potluck-btn"
         >
           Ask a Question →
         </a>
+        </span>
+    </div>
+    <div className="header__right">
+      <div className="title">
+        <h2 className="tagline"></h2>
+        
       </div>
       <div className="people">
         {/* <div className="person">
---
+++ shows/001 - Birth of a Database.md
@@ -0,0 +1,17 @@
+---
+number: 001
+title: Birth of a Database
+date: 153888000
+url: https://traffic.libsyn.com/syntax/Syntax000.mp3?dest-id=532671
+---
+
+## Show Notes
+
+In today's show, we'll cover the back story of the popular MongoDB database. How did it get started, why would someone create another database?
+
+### Questions
+1. Who created MongoDB?
+
+1. When was it created?
+
+1. What problems did MongoDB solve - what were the initial set of requirements?
